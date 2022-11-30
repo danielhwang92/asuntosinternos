3d44edbbb132a8403a185e0de6b9a0ae63ac4c43
Samuel Hwang <shwang@bomberosquito.gob.ec>
2022-11-30 10:56:36 -0500

    Initial commit

---
+++ asuntosinternos-ejb/src/main/java/ec/bomberosquito/asuntosinternos/beans/NewSessionBean.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/J2EE/EJB30/StatelessEjbClass.java to edit this template
+ */
+package ec.bomberosquito.asuntosinternos.beans;
+
+import javax.ejb.Stateless;
+import javax.ejb.LocalBean;
+
+/**
+ *
+ * @author danielhwang
+ */
+@Stateless
+@LocalBean
+public class NewSessionBean {
+
+    // Add business logic below. (Right-click in editor and choose
+    // "Insert Code > Add Business Method")
+}
